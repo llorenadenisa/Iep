135ba7215098f00a8b091a07563361b7f0804e33
llorenadenisa <48672926+llorenadenisa@users.noreply.github.com>
2020-12-09 22:42:22 +0200

    First 2 requirements implemented

---
+++ Pasari.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include <list> 
+#include <iterator> 
+
+using namespace std;
+
+class Pasare
+{  
+    protected:
+        string sunet;
+        int distanta;
+        Pasare(string sunet, int distanta):sunet(sunet),distanta(distanta){}
+    public:
+        virtual void getDistanta() = 0; 
+        virtual void getSunet() = 0;
+};
+
+class Papagal : public Pasare
+{
+    list <string> cuvinte_stiute; 
+    string sunetCarac;
+
+
+    public:
+        Papagal(string cuv, int dist, string& sunet):Pasare(sunet, dist){
+            cuvinte_stiute.push_back(cuv);
+        }
+
+        void adaugaCuvant(string cuvant_nou)
+        {
+            cuvinte_stiute.push_back(cuvant_nou);
+        }
+
+        void afiseazaCuvinteStiute()
+        {
+            for (const auto& it : cuvinte_stiute)
+            {
+                cout<<it<<" ";
+            }
+            cout<<"\n";
+        }
+
+        void getDistanta()
+        {
+            cout<<"Distanta parcursa de un papagal este: "<< distanta;
+            cout<<"\n";
+
+        }
+
+        void getSunet()
+        {
+            cout<<"Sunetul caracteristic al papagalului este: "<< sunet;
+            cout<<"\n";
+        }
+
+};
+
+int main(void)
+{
+    string s0("cuv1");
+    string s1("cuv2");
+    string sunet1("aaa");
+
+    Papagal papagal1(s0, 20, sunet1);
+    papagal1.adaugaCuvant(s1);
+    papagal1.afiseazaCuvinteStiute();
+    papagal1.getSunet();
+    papagal1.getDistanta();
+    return 0;
+
+}
